0a5c598141e06a9c98a059b4408f2402a11e49e6
root <pajinsu0803@naver.com>
2026-03-08 08:36:48 +0000

    shop/serializers.py OrderSerializer 작성

--- shop/serializers.py
+++ shop/serializers.py
@@ -5,3 +5,16 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('id', 'name', 'price', 'stock')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    item_id = serializers.PrimaryKeyRelatedField(
+        queryset=Item.objects.all(),
+        source='item',
+        write_only=True
+    )
+
+    class Meta:
+        model = Order
+        fields = ('id', 'item_id', 'status', 'created_at')
+        read_only_fields = ('id', 'status', 'created_at')
