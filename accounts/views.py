8764b11ec22fc249caf7a8fd3a384b53e8d4956f
root <pajinsu0803@naver.com>
2026-03-11 11:08:56 +0000

    feat: 회원가입, 로그인, 로그아웃 로직 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from .serializers import SignupSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "성공적으로 로그아웃 되었습니다."}, status=status.HTTP_205_RESET_CONTENT)
+        except:
+            return Response({"error": "잘못된 토큰입니다."}, status=status.HTTP_400_BAD_REQUEST)
--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_spectacular',
+    'rest_framework_simplejwt.token_blacklist',
 
     'accounts',
 ]
