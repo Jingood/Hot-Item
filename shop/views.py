594c95993777c38f6ef6844eb10425173e794b74
root <pajinsu0803@naver.com>
2026-03-13 08:39:34 +0000

    shop/views.py ItemListView 추가 - 상품 목록 기능

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from django.db import transaction
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class ItemListView(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [AllowAny]
+
+
