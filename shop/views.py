cf81fa3860e6d3dfae24b49a77f34cad419275aa
root <pajinsu0803@naver.com>
2026-03-14 08:46:02 +0000

    shop/views.py OrderCreateView 추가 - 상품 구매 기능

--- shop/views.py
+++ shop/views.py
@@ -13,3 +13,35 @@ class ItemListView(generics.ListAPIView):
     permission_classes = [AllowAny]
 
 
+class OrderCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    from drf_spectacular.utils import extend_schema
+    @extend_schema(request=OrderSerializer, responses={201: OrderSerializer})
+    def post(self, request, *args, **kwargs):
+        serializers = OrderSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+
+        item = serializers.validated_data['item']
+
+        # ? v1, 일반적인 RDBMS 조회 -> 예상 효과: 수천 명이 동시에 실행하면 모두가 stock > 0 이라고 판단 (초과 판매 발생)
+        if item.stock > 0:
+            item.stock -= 1
+            item.save()
+
+            order = Order.objects.create(
+                user=request.user,
+                item=item,
+                status=Order.Status.COMPLETED
+            )
+
+            return Response(
+                {"message": "주문이 완료되었습니다.", "order_id": order.id, "remain_stock": item.stock},
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                {"error": "재고가 소진되었습니다."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
