0052a975dff12d38858ff9080d204deae9e339eb
root <pajinsu0803@naver.com>
2026-03-17 08:48:49 +0000

    config/urls.py shop url 연결

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/accounts/', include('accounts.urls')),
+    path('api/shop/', include('shop.urls')),
 ]
