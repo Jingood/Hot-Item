7d91cf0f59b4387cb3116042223bfc97f5788900
root <pajinsu0803@naver.com>
2026-03-21 08:58:59 +0000

    locustfile.py 수정

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, task, between
+import random
 
 class HotItemShopper(HttpUser):
     wait_time = between(0.5, 1.0)
@@ -6,22 +7,24 @@ class HotItemShopper(HttpUser):
     
     def on_start(self):
         """
-        가상 유저가 생성되자마자 가장 먼저 실행되는 로직 (로그인)
+        가상 유저 생성 시 1~500번 중 랜덤으로 한 명의 유저를 선택하여 로그인
         """
-
+        user_id = random.randint(1, 500)
         login_data = {
-            "username": "admin",
-            "password": "admin1234"
+            "username": f"dumy{user_id}",
+            "password": "testpass123!"
         }
 
         response = self.client.post("/api/accounts/login/", json=login_data)
         if response.status_code == 200:
             self.token = response.json().get("access")
+        else:
+            print(f"로그인 실패: dummy{user_id}")
 
     @task
     def buy_limited_item(self):
         """
-        메인 부하 테스트 타겟: 나이키 조던 1 (item_id: 1) 구매 버튼 연타
+        실제 부하 테스트: 각기 다른 유저들이 하나의 상품(ID: 1)을 동시에 구매 시도
         """
         if self.token:
             headers = {
@@ -30,12 +33,12 @@ class HotItemShopper(HttpUser):
             payload = {
                 "item_id": 1
             }
-            with self.client.post("/api/shop/orders/", json=payload, headers=headers, catch_response=True) as response:
+
+            with self.client.post("/api/shop/orders", json=payload, headers=headers, catch_response=True) as response:
                 if response.status_code == 201:
                     response.success()
                 elif response.status_code == 400:
                     response.success()
                 else:
-                    response.failure(f"Unexpected status code: {response.status_code}")
+                    response.failure(f"에러 발생: {response.status_code}")
     ""
-    
